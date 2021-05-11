c4eb41a6445ce7f56a2a9f0259a834317cc2e483
Víctor Santoja <victorsantoja@gmail.com>
2021-05-11 18:43:45 +0200

    Add map_building file that use spanish cadastre to obtain buildings information and create a map

---
+++ R/map_building.r
@@ -0,0 +1,125 @@
+#' Using the information of cadastre, generate map with the building age information
+#'
+
+#' @export
+#' @import tidyverse feedeR sf lubridate fs tmap classInt showtext sysfonts rvest
+#' @examples create_spanish_cad_building_age(String city)
+
+create_spanish_cad_building_age <- function(city) {
+    
+    # load packages
+    library(feedeR)
+    library(sf) 
+    library(fs)
+    library(tidyverse)
+    library(lubridate)
+    library(classInt)
+    library(tmap)
+    library(rvest)
+
+    url <- "http://www.catastro.minhap.es/INSPIRE/buildings/ES.SDGC.bu.atom.xml"
+
+    # import RSS feed with provincial links
+    prov_enlaces <- feed.extract(url)
+    str(prov_enlaces)
+
+    # extract the table with the links
+    prov_enlaces_tab <- as_tibble(prov_enlaces$items) %>% 
+                        mutate(title = repair_encoding(title))
+
+    # filter the province and get the RSS link
+    atom <- filter(prov_enlaces_tab, str_detect(title, city)) %>% pull(link)
+
+    # import the RSS
+    enlaces <- feed.extract(atom)
+
+    # get the table with the download links
+    enlaces_tab <- enlaces$items
+    enlaces_tab <- mutate(enlaces_tab, title = repair_encoding(title), link = repair_encoding(link)) 
+
+    pattern_city <- paste("-", toupper(city), sep="")
+
+    link_city <- filter(enlaces_tab, str_detect(title, pattern_city)) %>% pull(link)
+
+    # download the data
+    download.file(link_city, temp <- tempfile(), method = "libcurl", quiet = TRUE, cacheOK = TRUE)
+
+    # unzip to a folder called buildings
+    unzip(temp, exdir = "buildings")
+
+    # get the path with the file
+    file_buildings <- dir_ls("buildings", regexp = "building.gml")
+
+    # import the data
+    buildings <- mutate(st_read(file_buildings), 
+                beginning = str_replace(beginning, "^-", "0000") %>% 
+                                ymd_hms() %>% as_date()
+                )
+
+    #font download
+    sysfonts::font_add_google("Montserrat", "Montserrat")
+
+    #use showtext for fonts
+    showtext::showtext_auto()
+
+    filter(buildings, beginning >= "1750-01-01")
+
+    # get the coordinates of the city
+    ciudad_point <- tmaptools::geocode_OSM(city, as.sf = TRUE)
+
+    #  project the points
+    ciudad_point <- st_transform(ciudad_point, 25830)
+
+    # create the buffer
+    point_bf <- st_buffer(ciudad_point, 2500)
+
+    # get the intersection between the buffer and the building
+    buildings_25 <- st_intersection(buildings, point_bf)
+
+    # find 15 classes
+    br <- classIntervals(year(buildings_25$beginning), 15, "quantile")
+
+    # create labels
+    lab <- names(print(br, under = "<", over = ">", cutlabels = FALSE))
+
+    # categorize the year
+    buildings_25 <- mutate(buildings_25, yr_cl = cut(year(beginning), br$brks, labels = lab, include.lowest = TRUE))
+
+    # colours
+    col_spec <- RColorBrewer::brewer.pal(11, "Spectral")
+
+    # colour ramp function
+    col_spec_fun <- colorRampPalette(col_spec)
+
+    print("Creating building map")
+    # create the map
+    final_map <- tm_shape(buildings_25) +
+        tm_polygons("yr_cl", 
+                border.col = "transparent",
+                palette = col_spec_fun(15),
+                textNA = "Without data",
+                title = "") +
+        tm_layout(bg.color = "black",
+            outer.bg.color = "black",
+            legend.outside = TRUE,
+            legend.text.color = "white",
+            legend.text.fontfamily = "Montserrat", 
+            panel.label.fontfamily = "Montserrat",
+            panel.label.color = "white",
+            panel.label.bg.color = "black",
+            panel.label.size = 5,
+            panel.label.fontface = "bold")
+
+    print("Saving map")
+
+    tmap_save(final_map, 
+            filename = paste(city, "_evolution_urban.png", sep=""),
+            scale = 1, 
+            width = 36, 
+            height = 24, 
+            units = "in",
+            dpi = 500)
+    
+    print("All done")
+
+}
--- README.md
+++ README.md
@@ -1,10 +1,33 @@
 
-# fuzzymapbuildings
+# Fuzzy map buildings
 
-<!-- badges: start -->
-<!-- badges: end -->
+INSPIRE is "an EU initiative to establish an infrastructure for spatial information in Europe that is geared to help to make spatial or geographical information more accessible and interoperable for a wide range of purposes supporting sustainable development".
 
-The goal of fuzzymapbuildings is to ...
+Spanish cadastre INSPIRE page -> http://www.catastro.meh.es/webinspire/index.html
+
+## Gradle Plugin (https://github.com/umayrh/gradle-plugin-r)
+```
+R setup, build and packaging tasks
+----------------------------------
+document - Creates documentation for R package
+installDeps - Installs common packaging dependencies
+packageBuild - Builds an R package into a tarball
+packageCheck - Runs checks for an R package
+packageClean
+packratClean - Removes packages (compiled and sources) managed by Packrat for an R package
+packratRestore - Restores packages managed by Packrat for R package
+renvClean - Removes packages (compiled and sources) managed by Renv for an R package
+renvRestore - Restores packages managed by Renv for R package
+restore - Restores all dependencies for a package.
+setup - Sets up a skeleton R package (warning: non-idempotent task).
+setupCreate - Ensures that pre-conditions for package setup are satisfied.
+setupPackrat - Sets up a skeleton R package using Renv (warning: non-idempotent task).
+setupRenv - Sets up a skeleton R package using Renv (warning: non-idempotent task)
+test - Runs test for an R package
+```
+
+## Some dependencies
+
 
 ## Installation
 
@@ -13,13 +36,18 @@ You can install the released version of fuzzymapbuildings from [CRAN](https://CR
 ``` r
 install.packages("fuzzymapbuildings")
 ```
+or
+```R
+devtools::install_github("vjsantojaca/fuzzy-map-buildings")
+```
 
 ## Example
 
-This is a basic example which shows you how to solve a common problem:
-
 ``` r
 library(fuzzymapbuildings)
-## basic example code
+
+create_spanish_cad_building_age("Salamanca")
 ```
 
+## Bibliography
+- https://dominicroye.github.io/en/2019/visualize-urban-growth/
